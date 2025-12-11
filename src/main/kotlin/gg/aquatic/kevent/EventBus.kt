14a59816da73f469353293e046fbe5393e921a02
MrLarkyy_ <studiotadikl@gmail.com>
2025-12-11 09:46:05 +0100

    Add inline helper methods for event subscription with detailed documentation

--- src/main/kotlin/gg/aquatic/kevent/EventBus.kt
+++ src/main/kotlin/gg/aquatic/kevent/EventBus.kt
@@ -26,11 +26,28 @@ interface EventBus {
     fun getEventExceptionHandler(): EventExceptionHandler
 }
 
+/**
+ * Subscribes to an event type, registering the provided listener to handle events of that type.
+ *
+ * @param T The type of event to subscribe to.
+ * @param listener The event listener that will be invoked when an event of the specified type is published.
+ * @param priority The priority level of the listener, defining its execution order relative to other listeners. Defaults to `EventPriority.NORMAL`.
+ * @return A `Subscription` representing the registration, which can be used to manage or unregister the listener.
+ */
 inline fun <reified T> EventBus.subscribe(
     listener: EventListener<T>,
     priority: EventPriority = EventPriority.NORMAL
 ): Subscription<T> = subscribe(T::class.java, listener, priority)
 
+/**
+ * Subscribes a listener to a specific event type with a weak reference,
+ * allowing the listener to be garbage collected if no strong references exist to it.
+ *
+ * @param T The type of event the listener will handle.
+ * @param listener The event listener to be invoked when events of the specified type are published.
+ * @param priority The priority of the subscription, determining the order of execution relative to other listeners. Defaults to `EventPriority.NORMAL`.
+ * @return A `Subscription` object representing the subscription, which can be used to unregister or query the listener's status.
+ */
 inline fun <reified T> EventBus.subscribeWeak(
     listener: EventListener<T>,
     priority: EventPriority = EventPriority.NORMAL
