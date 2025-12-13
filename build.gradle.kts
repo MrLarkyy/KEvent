ce9aa4297006b5ea47b35b8ce71281a345dfb296
MrLarkyy_ <studiotadikl@gmail.com>
2025-12-13 09:57:08 +0100

    Integrate `.env` configuration and Maven publishing setup. Update `.gitignore` for `.idea` and `.env` files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.3.0-RC3"
+    id("co.uzzu.dotenv.gradle") version "2.0.0"
+    `maven-publish`
 }
 
 group = "gg.aquatic.kevent"
@@ -16,4 +18,34 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val maven_username = if (env.isPresent("MAVEN_USERNAME")) env.fetch("MAVEN_USERNAME") else ""
+val maven_password = if (env.isPresent("MAVEN_PASSWORD")) env.fetch("MAVEN_PASSWORD") else ""
+
+publishing {
+    repositories {
+        maven {
+            name = "aquaticRepository"
+            url = uri("https://repo.nekroplex.com/releases")
+
+            credentials {
+                username = maven_username
+                password = maven_password
+            }
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "gg.aquatic"
+            artifactId = "KEvent"
+            version = "${project.version}"
+
+            from(components["java"])
+            //artifact(tasks.compileJava)
+        }
+    }
 }
