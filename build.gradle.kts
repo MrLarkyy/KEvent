37ef6bab3b32174a4269edc6414f44943d92e4b7
MrLarkyy_ <studiotadikl@gmail.com>
2025-12-21 13:41:23 +0100

    Make `handle` method suspendable in `EventListener`, bump version to 1.0.3.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "gg.aquatic.kevent"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/gg/aquatic/kevent/EventListener.kt
+++ src/main/kotlin/gg/aquatic/kevent/EventListener.kt
@@ -3,6 +3,6 @@ package gg.aquatic.kevent
 fun interface EventListener<T> {
 
     @Throws(Exception::class)
-    fun handle(event: T)
+    suspend fun handle(event: T)
 
 }
